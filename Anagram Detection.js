c840ddef0b6798744ad7e503be42bd51a0ef35a9
Fatemeh Nazari <fatemehnazari13770111@gmail.com>
2021-06-05 06:56:16 +0430

    Anagram Detection

--- Anagram Detection.js
+++ Anagram Detection.js
@@ -16,6 +16,14 @@ var isAnagram = function(test, original) {
   return (test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join(''));
 };
 
+//OR 
+
+var isAnagram = function (test, original) {
+  var t = test.toLowerCase().split("").sort().join("");
+  var o = original.toLowerCase().split("").sort().join("");
+  return t == o ? true : false;
+};
+
 // =====================================================================================================//
 
 // Sample Tests:
